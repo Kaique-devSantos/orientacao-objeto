3f4e0ba5fb1f371e3f7f83099387a67862e5f3f1
Kaique-devSantos <kaiqueads.2023@gmail.com>
2024-05-02 18:08:17 -0300

    entende e trabalhando conceito de propriedades privadas

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import User from "./User.js";
-import Admin from "./Admin.js";
 import Docente from "./docente.js";
+import Admin from "./Admin.js";
+
+
+const novoUser = new User('Mariana', 'm@m.com', '2021-01-01');
+
+console.log(novoUser.exibirInfos());
+
+// novoUser.#nome = 'Marcia';
